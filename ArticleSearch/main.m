20dae736bd6dbc27ad190c7588a4ee9735c3b50d
Jordan Mazurke <jordan@jzmdevelopment.co.uk>
2015-08-25 21:11:26 +0100

    Initial Commit

---
+++ ArticleSearch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArticleSearch
+//
+//  Created by Jordan Mazurke on 25/08/2015.
+//  Copyright (c) 2015 Jordan Mazurke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
